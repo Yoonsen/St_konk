d4003312510ee44601a2b20c1dbbcbe902fa4cbb
yoonsen <yoonsen@gmail.com>
2024-04-30 17:14:49 +0200

    done

--- concordances.py
+++ concordances.py
@@ -21,7 +21,7 @@ def to_excel(df):
     writer = pd.ExcelWriter(output, engine='openpyxl')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
     writer.close()
-    worksheet = writer.sheets['Sheet1']
+    #worksheet = writer.sheets['Sheet1']
     processed_data = output.getvalue()
     return processed_data
 
@@ -44,8 +44,8 @@ def concordance(
     :param int limit: max. number of concordances per document. Maximum value is 1000.
     :return: a table of concordances
     """
-    if words is None:
-        return {}  # exit condition
+    if words is None or words == '':
+        return pd.DataFrame()  # exit condition
     else:
         #st.write("antall urner:", len(urns))
         params = {"dhlabids": dhlabids, "query": words, "window": window, "limit": limit}
@@ -80,33 +80,41 @@ def show_concs(corpus, concs, search, number):
         html = "%s %s" % (link, conc_markdown)
         st.markdown(html, unsafe_allow_html=True)
 
-corpusfile = st.file_uploader("Last opp et korpus", help="Dra en fil over hit, fra et nedlastningsikon, eller velg fra en mappe")
-if corpusfile is not None:
-    corpus_defined = True
-    dataframe = pd.read_excel(corpusfile)
-    corpus = dh.Corpus(doctype='digibok',limit=0)
-    corpus.extend_from_identifiers(list(dataframe.urn))
-    corpus = corpus.frame
 
-col1, col2, col3, col4 = st.columns(4)
+col1, col2, col3, col4, cold,  = st.columns([3,2,2,2,5])
+
+with cold:
+    corpusfile = st.file_uploader("Last opp et korpus", help="Dra en fil over hit, fra et nedlastningsikon, eller velg fra en mappe")
+    if corpusfile is not None:
+        corpus_defined = True
+        dataframe = pd.read_excel(corpusfile)
+        corpus = dh.Corpus(doctype='digibok',limit=0)
+        corpus.extend_from_identifiers(list(dataframe.urn))
+        corpus = corpus.frame
+
+
 with col1:
     konk_ord = st.text_input("konkordans for", st.session_state.get('conc_word', ''), key='conc_word')
 with col2:
-    antall = st.number_input("maks antall konkordanser", min_value = 1, max_value = 5000, value = st.session_state.get('conc_numbers', 1000), key='conc_numbers')
+    antall = st.number_input("maks antall", min_value = 1, max_value = 5000, value = st.session_state.get('conc_numbers', 1000), key='conc_numbers', help="maks antall konkordanser")
 with col3:
-    kontekst = st.number_input('konkordansevindu', min_value = 1, max_value = 100, value = 25, key = 'conc_window')
+    kontekst = st.number_input('konkordansevindu', min_value = 1, max_value = 100, value = 25, key = 'conc_window', help="antallet ord konkordansen skal vise")
 with col4:
-    limit = st.number_input("antall konkordanser som vises", min_value=5, max_value = 200, value=50)
+    limit = st.number_input("antall konkordanser", min_value=5, max_value = 5000, value=50, help="hvor mange skal vises i gangen")
 
 concs = concordance(list(corpus.dhlabid.values), words=konk_ord, limit=antall, window=kontekst)
 
 st.write(f"Fant {len(concs)} konkordanser viser {min(len(concs), limit)} av dem")
 
-st.write("# Konkordanser med årstall og lenke - klikk på lenken for å se på teksten")
+st.write("### Konkordanser med årstall og lenke")
+st.write("Nedlastningsknapp under konkordansene")
+st.write("---")
+
 show_concs(corpus, concs, konk_ord,limit)
 
-excel_fil = to_excel(concs.merge(corpus, left_on='urn', right_on='urn')[["urn","year","conc"]])
+st.write("---")  
 
+excel_fil = to_excel(concs.merge(corpus, left_on='urn', right_on='urn')[["urn","year","timestamp", "conc"]])
 filnavn = st.text_input("Filnavn for excelfil", "konkordanser.xlsx")
 st.download_button(
     label = f"Last ned alle {len(concs)} konkordansene ",
